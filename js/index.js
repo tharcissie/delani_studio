24025a9848e7709f56c81d3e96900e149d1fb2eb
tharcissie <tharcissieidufashe@gmail.com>
2020-09-30 21:14:19 +0200

    added a submit button

--- js/index.js
+++ js/index.js
@@ -1,35 +1,31 @@
-
-$('.icon1').click(function () {
+$('.icon1').click(function() {
     $('#icon1').show();
     $(".icon1").hide();
- 
+
 });
-$('#icon1').click(function () {
+$('#icon1').click(function() {
     $('.icon1').show();
     $("#icon1").hide();
- 
+
 });
-$('.icon2').click(function () {
+$('.icon2').click(function() {
     $('#icon2').show();
     $(".icon2").hide();
- 
+
 });
-$('#icon2').click(function () {
+$('#icon2').click(function() {
     $('.icon2').show();
     $("#icon2").hide();
- 
+
 });
 
-$('.icon3').click(function () {
+$('.icon3').click(function() {
     $('#icon3').show();
     $(".icon3").hide();
- 
+
 });
-$('#icon3').click(function () {
+$('#icon3').click(function() {
     $('.icon3').show();
     $("#icon3").hide();
- 
+
 });
-
-
-
